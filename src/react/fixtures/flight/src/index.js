549b1ef23324860df9cb80175e147fff3b8c7186
bubucuo <276185876@qq.com>
2020-03-09 15:02:02 +0800

    clear

--- src/react/fixtures/flight/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactFlightDOMClient from 'react-flight-dom-webpack';
-import App from './App';
-
-let data = ReactFlightDOMClient.readFromFetch(fetch('http://localhost:3001'));
-ReactDOM.render(<App data={data} />, document.getElementById('root'));
--- src/react/packages/shared/ReactVersion.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-// TODO: this is special because it gets imported during build.
-export default '16.13.0';
