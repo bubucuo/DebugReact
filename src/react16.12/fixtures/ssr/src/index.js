5c883356fe41caff066bbb5414d141b8956b83b0
bubucuo <276185876@qq.com>
2020-03-27 15:59:55 +0800

    del 16.12

--- src/react16.12/fixtures/ssr/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {createRoot} from 'react-dom';
-
-import App from './components/App';
-
-let root = createRoot(document, {hydrate: true});
-root.render(<App assets={window.assetManifest} />);
